0747f9cbb64855f04bb6bfd3515166e788b930ab
Vyacheslav Pustovit <pustovit.slava@gmail.com>
2021-06-07 22:29:41 +0300

    fix bags

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function () {
+  
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-logo').toggleClass('header__top-logo--active');
+    $('.header__top-menu-items').toggleClass('header__top-menu-items--active');
+  });
+
   $('.header__top-menu-link').on('click', function () {
     $('.header__top-menu-link').removeClass('header__top-menu-link--active');
     $(this).addClass('header__top-menu-link--active');
@@ -27,4 +33,24 @@ $(function () {
     adaptiveHeight: true,
     focusOnSelect: true,
   });
+
+
+  $(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 1500);
+    });
+  });
+
+  $(document).ready(function () {
+    $("#header__content").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 1500);
+    });
+  });
+  
 });
